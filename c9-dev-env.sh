82306dae215ff76894a4a64d578e0f66343bc7d6
Lyon Liang <lyon.liang12@gmail.com>
2016-02-19 02:59:56 +0000

    add git configuration

---
+++ c9-dev-env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git config --global user.name "Lyon Liang"
+git config --global user.email lyon.liang12@gmail.com
+git config --global core.editor vim
+git config --global alias.st status
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.up rebase
+git config --global alias.ci commit
